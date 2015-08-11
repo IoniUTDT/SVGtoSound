14b9b4e3a253c7768d25c3bc85d86c5e9b21cfc3
IoniUTDT <ionatanlist@gmail.com>
2015-08-11 19:50:08 -0300

    Parece estar andando!!! :) Faltan algunos detalles como el tukeywin y creo que hace lio cuando hay varias lineas...

--- SVGtoSound/src/com/turin/tur/Main/SVGtoSound.java
+++ SVGtoSound/src/com/turin/tur/Main/SVGtoSound.java
@@ -22,13 +22,17 @@ public class SVGtoSound {
 	public String path = "./testFiles/";
 	public File[] archivosOriginales;
 	public ArrayList<InfoArchivo> archivos = new ArrayList<InfoArchivo>();
-	public final boolean logScale=true;
-	public final boolean fixScale=true;
-	public final float maxHeigth=100;
+	public final boolean logScale = true;
+	public final boolean fixScale = true;
+	public final float maxHeigth = 100;
 	public final float frecMax = 8000;
 	public final float frecMin = 100;
-	
-	
+	public final float time = 5; // in secs
+	public final boolean fixedTime = true; // indicate if the length of sound must be the variable time
+	public final float secByPix = 5 / 100f; // indicate how many sec are represented by pixel.
+	public final int fs = 44100; // hz of the sound
+	public final float base = 10; // base of the log scale
+
 	public SVGtoSound() {
 		this.loadFiles();
 		this.createSounds();
@@ -37,180 +41,231 @@ public class SVGtoSound {
 	/**
 	 * Create a sound that change in frecuencie in logaritmic form
 	 * 
-	 *  
-	 * @param freci Inicial value of frecuence
-	 * @param frecf Final value of frecuence
-	 * @param T Time (in sec) of sound
-	 * @param fs samples per second of the sound 
-	 * @return 
+	 * 
+	 * @param freci
+	 *            Inicial value of frecuence
+	 * @param frecf
+	 *            Final value of frecuence
+	 * @param T
+	 *            Time (in sec) of sound
+	 * @param fs
+	 *            samples per second of the sound
+	 * @return
 	 * 
 	 */
-	private double[] createMusicRamp (float freci, float frecf, float T, float fs) {
+	private double[] createMusicRamp(float freci, float frecf, float ti, float tf) {
 		double dt = 1 / fs; // es el dt que transcurre entre sample y sample
-		int N = Math.round(T * fs); // El numero de samples que hay que crear
-		double[] frec = logspacelog(freci, frecf, N, 10); // Crea una escala logaritmica en base 10 que va de la frecuencia inicial a la final
-		for(int i=0 ; i<frec.length ; i++) { // Lo multiplica x 2pi para trabajar con la fase
-			frec[i] = frec[i]*2*Math.PI;
+		int N = Math.round((tf - ti) * fs); // El numero de samples que hay que crear
+		double[] frec = logspacelog(freci, frecf, N, base); // Crea una escala logaritmica en base 10 que va de la frecuencia inicial a la final
+		for (int i = 0; i < frec.length; i++) { // Lo multiplica x 2pi para trabajar con la fase
+			frec[i] = frec[i] * 2 * Math.PI;
 		}
 		// Integra las freciencias instantaneas
-		for(int i=1 ; i<frec.length ; i++) { // Lo multiplica x 2pi para trabajar con la fase
-			frec[i] = frec[i]+frec[i-1]*dt; // El primero lo deja tal cual y despues suma hasta el ultimo
-		} 
+		for (int i = 1; i < frec.length; i++) { // Lo multiplica x 2pi para trabajar con la fase
+			frec[i] = frec[i] + frec[i - 1] * dt; // El primero lo deja tal cual y despues suma hasta el ultimo
+		}
 		// ahora frec es la fase instante a instante
-		
+
 		// Vamos a hacer el coseno de la fase
-		for(int i=1 ; i<frec.length ; i++) {
-			frec[i] = Math.cos(frec[i]); 
-		} 
+		for (int i = 1; i < frec.length; i++) {
+			frec[i] = Math.cos(frec[i]);
+		}
 		// Ya esta creado el sonido
-		
+
 		// ACA hay que agregar la rampoa tukeywin
 
 		return frec;
-		
-		int sampleRate = (int) fs;    // Samples per second
-        double duration = T;     // Seconds
 
-        // Calculate the number of frames required for specified duration
-        long numFrames = (long)(duration * sampleRate);
-
-        File file = new File (path,"prueba.wav");
-        
-        // Create a wav file with the name specified as the first argument
-        try {
-			WavFile wavFile = WavFile.newWavFile(file, 1, numFrames, 16, sampleRate);
-			wavFile.writeFrames(frec, frec.length);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (WavFileException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// Ahora vamos a grabarla a un archivo
-		// WavFile wavFile = WavFile.newWavFile(new File(args[0]), 2, numFrames, 16, sampleRate);
-		
 	}
 
 	/**
 	 * log in any base
 	 * 
-	 * @param base The base of log
-	 * @param num The argument of log
+	 * @param base
+	 *            The base of log
+	 * @param num
+	 *            The argument of log
 	 * @return
 	 */
 	public double logOfBase(double base, double num) {
-	    return Math.log(num) / Math.log(base);
+		return Math.log(num) / Math.log(base);
 	}
-	
-	/**
-     * generates n logarithmically-spaced points between d1 and d2 using the
-     * provided base. d1 and d2 are the expected start and end.
-     * 
-     * @param d1 The min value expected
-     * @param d2 The max value expected
-     * @param n The number of points to generated
-     * @param base the logarithmic base to use
-     * @return an array of lineraly space points.
-     */
-	
-    public strictfp double[] logspacelog(double d1, double d2, int n, double base) {
-        double[] y = new double[n];
-        double[] p = linspace(logOfBase(base,d1), logOfBase(base,d2), n);
-        for(int i = 0; i < y.length - 1; i++) {
-            y[i] = Math.pow(base, p[i]);
-        }
-        y[y.length - 1] = Math.pow(base, logOfBase(base,d2));
-        return y;
-    }
 
 	/**
-     * generates n logarithmically-spaced points between d1 and d2 using the
-     * provided base.
-     * 
-     * Nota! Esto parece estar muy mal!
-     * 
-     * @param d1 The min value
-     * @param d2 The max value
-     * @param n The number of points to generated
-     * @param base the logarithmic base to use
-     * @return an array of lineraly space points.
-     */
-	
-    public strictfp double[] logspace(double d1, double d2, int n, double base) {
-        double[] y = new double[n];
-        double[] p = linspace(d1, d2, n);
-        for(int i = 0; i < y.length - 1; i++) {
-            y[i] = Math.pow(base, p[i]);
-        }
-        y[y.length - 1] = Math.pow(base, d2);
-        return y;
-    }
+	 * generates n logarithmically-spaced points between d1 and d2 using the provided base. d1 and d2 are the expected start and end.
+	 * 
+	 * @param d1
+	 *            The min value expected
+	 * @param d2
+	 *            The max value expected
+	 * @param n
+	 *            The number of points to generated
+	 * @param base
+	 *            the logarithmic base to use
+	 * @return an array of lineraly space points.
+	 */
 
-    /**
-     * generates n linearly-spaced points between d1 and d2.
-     * @param d1 The min value
-     * @param d2 The max value
-     * @param n The number of points to generated
-     * @return an array of lineraly space points.
-     */
-    public static strictfp double[] linspace(double d1, double d2, int n) {
+	public strictfp double[] logspacelog(double d1, double d2, int n, double base) {
+		double[] y = new double[n];
+		double[] p = linspace(logOfBase(base, d1), logOfBase(base, d2), n);
+		for (int i = 0; i < y.length - 1; i++) {
+			y[i] = Math.pow(base, p[i]);
+		}
+		y[y.length - 1] = Math.pow(base, logOfBase(base, d2));
+		return y;
+	}
 
-        double[] y = new double[n];
-        double dy = (d2 - d1) / (n - 1);
-        for(int i = 0; i < n; i++) {
-            y[i] = d1 + (dy * i);
-        }
+	/**
+	 * generates n linearly-spaced points between d1 and d2.
+	 * 
+	 * @param d1
+	 *            The min value
+	 * @param d2
+	 *            The max value
+	 * @param n
+	 *            The number of points to generated
+	 * @return an array of lineraly space points.
+	 */
+	public static strictfp double[] linspace(double d1, double d2, int n) {
 
-        return y;
+		double[] y = new double[n];
+		double dy = (d2 - d1) / (n - 1);
+		for (int i = 0; i < n; i++) {
+			y[i] = d1 + (dy * i);
+		}
 
-    }
-    
-	private void createSounds() {
-		if (archivos!=null) {
+		return y;
+
+	}
+
+	/**
+	 * Create the secuence of mp3 from the info in files
+	 */
+	public void createSounds() {
+
+		if (archivos != null) {
 			for (InfoArchivo archivo : archivos) {
-				// Primero creamos la rampa principal
-				
+
+				float timefactor;
+				if (fixedTime) {
+					timefactor = time / (secByPix * archivo.ancho);
+				} else {
+					timefactor = 1;
+				}
+
+				// create the base full length secuence for file
+				int N = (int) (secByPix * archivo.ancho * timefactor * fs);
+				double[] secuence = new double[N];
+
+				for (Linea linea : archivo.lineas) {
+
+					double[] rampa = createMusicRamp(linea.freci, linea.frecf, linea.ti, linea.tf);
+					int frameInicial = (int) linea.ti * fs;
+					for (int i = 0; i < rampa.length; i++) {
+						secuence[i + frameInicial] = secuence[i + frameInicial] + rampa[i];
+					}
+				}
+
+				File file = new File(path, "prueba.wav");
+				// Create a wav file with the name specified as the first argument
+				try {
+					WavFile wavFile = WavFile.newWavFile(file, 1, secuence.length, 16, fs);
+					wavFile.writeFrames(secuence, secuence.length);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (WavFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-		createMusicRamp(300,8000, 10, 44100);
+
+	}
+
+	private void findParameters(Linea linea, float ancho, float alto) {
+
+		// get scale correction for file
+		float timefactor;
+		if (fixedTime) {
+			timefactor = time / (secByPix * ancho);
+		} else {
+			timefactor = 1;
+		}
+		float frecfactor;
+		if (fixScale) {
+			frecfactor = maxHeigth / alto;
+		} else {
+			frecfactor = 1;
+		}
+
+		float xi = linea.xi * timefactor;
+		float xf = linea.xf * timefactor;
+		// get the initial and final frec based in conditions
+		float yi = linea.yi * frecfactor;
+		float yf = linea.yf * frecfactor;
+		// corrige que el SVG toma el cero arriba
+		yi = maxHeigth - yi;
+		yf = maxHeigth - yf;
+
+		linea.ti = xi * secByPix;
+		linea.tf = xf * secByPix;
+
+		if (logScale) {
+			/*
+			 * partimos de la forma frec = A*exp(y) + B y sabemos que frecMax = A*base^(maxHeigth) + B y que frecMin = A * base^(0) + B
+			 * 
+			 * haciendo cuentas queda que:
+			 * 
+			 * A = (FrecM - frecm) / (base^M-base^m) B = [ (frecM + frecm) - A (base^M + base^m) ] / 2
+			 * 
+			 * el problema es que se hacen numeros muy grandes de manejar por lo tanto vamos a reescalar el problema de manera que la yMax sea 1 de esta forma
+			 * nos aseguramos que nunca va a dar resultados inmanejables.
+			 */
+
+			double A = (frecMax - frecMin) / (Math.pow(base, 1) - Math.pow(base, 0));
+			double B = ((frecMax + frecMin) - A * (Math.pow(base, 1) + Math.pow(base, 0))) / 2;
+			linea.freci = (float) (A * Math.pow(base, yi / maxHeigth) + B);
+			linea.frecf = (float) (A * Math.pow(base, yf / maxHeigth) + B);
+		} else {
+			linea.freci = frecMin + yi * (frecMax - frecMin) / maxHeigth;
+			linea.frecf = frecMin + yf * (frecMax - frecMin) / maxHeigth;
+		}
 	}
 
 	public class InfoArchivo {
 		String nombre;
 		float ancho;
 		float alto;
-		
+
 		ArrayList<Linea> lineas = new ArrayList<Linea>();
 	}
-	
+
 	public class Linea {
+		// Parametros que se leen desde cada linea del SVG
 		float xi;
 		float xf;
 		float yi;
 		float yf;
+		// Parametros que se calculan en funcion de parametros generales de la imagen y del programa
 		float freci;
 		float frecf;
 		float ti;
 		float tf;
+
 	}
-	
+
 	private void loadFiles() {
-		
-		File home = new File("hola.txt");
-		System.out.println(home.getAbsolutePath());
 
 		// Primero busca la lista de archivos de interes
 		File dir = new File(path);
-		System.out.println(dir.getAbsolutePath());
 		archivosOriginales = dir.listFiles(new SvgFileFilter());
 
 		for (File file : archivosOriginales) { // El loop por cada archivo
 			// Crea la entrada perteneciente al archivo
 			InfoArchivo infoArchivo = new InfoArchivo();
 			infoArchivo.nombre = file.getName().replaceFirst("[.][^.]+$", "");
-			
+
 			try {
 
 				// Esto crea la info a partir del archivo
@@ -222,31 +277,31 @@ public class SVGtoSound {
 				// Recupera el ancho y el alto
 				infoArchivo.alto = Float.parseFloat(doc.getDocumentElement().getAttribute("height"));
 				infoArchivo.ancho = Float.parseFloat(doc.getDocumentElement().getAttribute("width"));
-				
+
 				// Recupera la lista de elementos de una categoria (en nuestro caso lineas)
 				NodeList nList = doc.getElementsByTagName("line");
-				for (int i = 0; i < nList.getLength(); i++) { 					// itera sobre cada linea
+				for (int i = 0; i < nList.getLength(); i++) { // itera sobre cada linea
 					if (nList.item(i).getNodeType() == Node.ELEMENT_NODE) { // Se fija que el nodo sea un elemento (si no entendi mal eso deferencia de los atributos, etc)
 						Linea linea = new Linea();
 						Element eElement = (Element) nList.item(i);
-						
+
 						linea.xi = Float.parseFloat(eElement.getAttribute("x1"));
 						linea.xf = Float.parseFloat(eElement.getAttribute("x2"));
 						linea.yi = Float.parseFloat(eElement.getAttribute("y1"));
 						linea.yf = Float.parseFloat(eElement.getAttribute("y2"));
-						
+
+						findParameters(linea, infoArchivo.ancho, infoArchivo.alto);
 						infoArchivo.lineas.add(linea);
 					}
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			// Agrega la info del archivo a la lista general
 			archivos.add(infoArchivo);
 		}
 	}
-	
 
 	public class SvgFileFilter implements FileFilter
 	{
